4e790628d2694e276f96ed0faf147588590b8d79
Kirill <mrbomberman99@gmail.com>
2021-07-20 23:51:47 +0300

    добавил динамичное ветвление таблицы

--- src/components/CarTableRow/index.js
+++ src/components/CarTableRow/index.js
@@ -1,13 +1,40 @@
-function CarTableRow({ item }) {
-  console.log(item.tariffs)
-  return (
-    <tr className="">
-      <td>{item.mark} / {item.model}</td>
+import {useState} from 'react';
+
+function CarTableRow({ item, tariffs, activeChange }) {
+    const [activeClass , setActiveClass] = useState(false)
+
+    // const toggleClass = (e) => {
+    //   if(e.target.className.includes('stylePanel')){
+    //     setActiveClass(activeClass)
+    //   }
       
      
+    // };
+
+  return (
+    <>
+    <tr className="styleRow">
+      <td>{item.mark} / {item.model}</td>
+      
+      {tariffs.map((tariff) => {
+        const itemTariff = item.tariffs[tariff];
+        const key = tariff + item.mark + item.model
+        if (!itemTariff) {
+          return <td key={key}>-</td>;
+        }
+        return <td className={activeClass ? 'active' : 'stylePanel'} onClick={() => {
+          activeChange(item.mark + ' ' + item.model + ' '+ itemTariff.year)
+
+          
+        }
+        }
+        key={key}>{itemTariff.year}</td>
+      })}
       
     </tr>
+    </>
   );
 }
 
 export default CarTableRow
+// ['Стандарт'||'Комфорт'||'Эконом'||'Комфорт+'||'Бизнес'||'Минивен'||'Лайт']
