46a0e35cea14d4d93729cb14e30dc0e702123127
Kirill <mrbomberman99@gmail.com>
2021-07-20 23:49:03 +0300

    добавил панель с активным текстом, который меняется в зависимости от клика на машину

---
+++ src/components/ActiveCarPanel/index.js
@@ -0,0 +1,13 @@
+function ActiveCarPanel({activeCar}){
+    if(activeCar === null){
+        return  (
+            <div className='panel'>Автомобиль не выбран</div>
+        )
+    } else {
+        return (
+            <div className='panel'>Выбран автомобиль {activeCar} года выпуска</div>
+        )
+    }
+}
+
+export default ActiveCarPanel;
